bce80f7929a629e2015803ae349378cf2ec99eee
pekinglc <liufly20000@hotmail.com>
2023-12-22 21:42:33 -0500

    d

--- src/main/java/com/fsd08/MediLink/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.fsd08.MediLink.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/fsd08/MediLink/registration/password/PasswordRestRequest.java
+++
@@ -1,4 +0,0 @@
-package com.fsd08.MediLink.registration.password;
-
-public class PasswordRestRequest {
-}
